79916f74345153e45dd1a7d667728afa7360c268
Shawn Xue <you@example.com>
2024-05-07 13:44:29 -0400

    aaa

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -3,7 +3,24 @@ import React from "react";
 const MainPage: React.FC<{}> = ({}) => {
 
     return (
-        <div>MainPage</div>
+        <div className="w-screen h-screen bg-red-400">
+            <div className="flex w-screen">
+                <div>AAAA</div>
+                <div className="flex-1"></div>
+                <div className="bg-blue-100 px-4 py-3 border border-gray-400">Home</div>
+                <div className="bg-blue-100 px-2 py-3 border border-gray-400">Account</div>
+                <div className="bg-blue-100 px-2 py-3 border border-gray-400">Recipes</div>
+            </div>
+
+            <div className="w-screen bg-green-300 flex">
+                <div className="w-1/3">
+                    a
+                </div>
+            
+                <div className="w-2/3">b</div>
+            </div>
+
+        </div>
     )
 }
 
